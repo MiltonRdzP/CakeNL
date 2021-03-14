b3b3fb1a93cd6ee3cc3bb7c1981889654f71602f
MiltonRdzP <miltonrdz25@hotmail.com>
2021-03-13 22:36:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
+import  {Route, BrowserRouter, Link} from 'react-router-dom';
 import './App.css';
-
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+import {FaBirthdayCake, FaShoppingCart, FaUserCircle} from 'react-icons/fa'
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>
+                &#9776;
+              </button>
+              <Link to="/"><FaBirthdayCake
+              style={{
+                marginRight:"10px",
+                marginLeft:"10px",
+                marginTop:"2px"
+              }}/>CakeNL</Link>
+            </div>
+            <div className="header-links">
+              <a href="cart.html"><FaShoppingCart
+              style={{
+                marginRight:"5px",
+                marginLeft:"5px",
+                marginTop:"2px"
+              }}/>Carrito de compras</a>
+              <a href="signin.html"><FaUserCircle
+              style={{
+                marginRight:"5px",
+                marginLeft:"5px",
+                marginTop:"2px"
+              }}/>Iniciar Sesión</a>
+            </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Categorias</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+              <li>
+                <a href="index.html">Pasteles</a>
+              </li>
+
+              <li>
+                <a href="index.html">Reposteria</a>
+              </li>
+
+            </ul>
+          </aside>
+          <main className="main">
+            <div className="content">
+              <Route path="/product/:id" component={ProductScreen}/>
+              <Route path="/" exact={true} component={HomeScreen} />
+              
+            </div>
+            </main>
+          <footer className="footer">
+            All right reserved.
+          </footer>
+        </div>
+    </BrowserRouter>
   );
 }
 
